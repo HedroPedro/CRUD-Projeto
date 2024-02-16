673272b60011f3bf5387f420d87c8ab21467a33a
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2024-02-16 15:33:23 -0300

    initiliaze feito

--- operations.c
+++ operations.c
@@ -1,9 +1,11 @@
 #include "operations.h"
 
+    uint qtdItens;
+
     FILE *initialize(remedio *remedios, char *path){
         FILE *fp = fopen(path, "w+");
-
-        int i = 1;
+        remedio remedioTmp;
+        int i = 0;
         remedios = (remedio *) malloc(sizeof(remedio));
         if(!fp){
         fprintf(stderr,"ERRO AO CRIAR ARQUIVO!");
@@ -11,15 +13,27 @@
         }
 
         while(!feof(fp)){
-            remedios = (remedio *) realloc(remedios, sizeof(remedio) * i);
-            fscanf(fp, "id: %u", &remedios[i-1].id);
-            fscanf(fp, "nome: %s", &remedios[i-1].nome);
-            fscanf(fp, "preco: %f", &remedios[i-1].preco);
-            fscanf(fp, "");
+            fscanf(fp, "id: %u", &remedioTmp.id);
+            fscanf(fp, "nome: %s", &remedioTmp.nome);
+            fscanf(fp, "preco: %f", &remedioTmp.preco);
+            fscanf(fp, "generico: %d", &remedioTmp.ehGenerico);
+            fscanf(fp, "quantidade: %u", &remedioTmp.qtd);
+            fscanf(fp, "categoria: %u", &remedioTmp.categoria);
+            fscanf(fp, "fabricante: %s", &remedioTmp.fabricante);
             i++;
+            remedios = (remedio *) realloc(remedios, sizeof(remedio) * i);
+            remedios[i-1] = remedioTmp;
         }
+        qtdItens = i;
+        return fp;
     }
-    void addRemedio(void){}
+
+    void addRemedio(void){
+        remedio remedioTmp;
+        printf("Digite ");
+    }
+
+
     void showAll(void){}
     void showRemedioById(void){}
     void updateRemedio(void){}
