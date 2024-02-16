80114bb27de92a2ffd6e50ee4853b3cb1f8af74d
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2024-02-16 14:37:44 -0300

    base

--- operations.c
+++ operations.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "operations.h"
+
+    typedef unsigned int uint;
+
+    typedef struct remedio{
+        uint id;
+        char *nome;
+        float preco;
+        uint qtd;
+        bool ehGenerico;
+        uint categoria;
+        char *fabricante;
+    };
+
+    void initialize(void){}
+    void addRemedio(void);
+    void showAll(void);
+    void showRemedioById(void);
+    void updateRemedio(void);
+    void removeRemedio(void);
